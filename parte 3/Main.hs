a821bd19a8b0ac32f1132acb9a6f4781895da438
Callegari Filippo <callegari.filippo@gmail.com>
2020-08-03 17:58:23 +0200

    cast from integer to double for type eq

--- parte 3/Main.hs
+++ parte 3/Main.hs
@@ -27,21 +27,21 @@ fromOk (Ok x) = x
 fromBad :: Err a -> String
 fromBad (Bad x) = x
 
--- saneHTree :: Num a => String -> Tree a
-{- saneHTree str = 
+saneHTree :: String -> Tree Double
+saneHTree str = 
                 let partialI = ((pTreeLexI . tokens) str)
                     partialD = ((pTreeLexD . tokens) str)
                 in  if (isOk partialD)
                     then (fromTLD2Tree (fromOk partialD) 0)
                     else (fromTLI2Tree (fromOk partialI) 0)
 
--}
-fromTLD2Tree :: TreeLexD -> Integer -> Tree a
+
+fromTLD2Tree :: TreeLexD -> Integer -> Tree Double
 fromTLD2Tree (LeafD d w) h = Leaf d w h
 fromTLD2Tree (TreeD d w ts) h = Node d w h $ (\x-> fromTLD2Tree x (h+1)) <$> ts
 
 
---fromTLI2Tree :: Num a => TreeLexI -> Integer -> Tree a
-fromTLI2Tree (LeafI d w) h = Leaf d w h
-fromTLI2Tree (TreeI d w ts) h = Node d w h $ (\x-> fromTLI2Tree x (h+1)) <$> ts
+fromTLI2Tree :: TreeLexI -> Integer -> Tree Double
+fromTLI2Tree (LeafI d w) h = Leaf (fromInteger d) w h
+fromTLI2Tree (TreeI d w ts) h = Node (fromInteger d) w h $ (\x-> fromTLI2Tree x (h+1)) <$> ts
 
