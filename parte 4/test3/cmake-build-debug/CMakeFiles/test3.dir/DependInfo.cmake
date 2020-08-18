b28b8898327633d2ec1d1873abb86f10581a1009
Callegari Filippo <callegari.filippo@gmail.com>
2020-08-18 17:21:18 +0200

    aggiunto makefile

---
+++ parte 4/test3/cmake-build-debug/CMakeFiles/test3.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/igor/Scrivania/haskell/progettoLC/parte 4/test3/Absyn.c" "/home/igor/Scrivania/haskell/progettoLC/parte 4/test3/cmake-build-debug/CMakeFiles/test3.dir/Absyn.c.o"
+  "/home/igor/Scrivania/haskell/progettoLC/parte 4/test3/Lexer.c" "/home/igor/Scrivania/haskell/progettoLC/parte 4/test3/cmake-build-debug/CMakeFiles/test3.dir/Lexer.c.o"
+  "/home/igor/Scrivania/haskell/progettoLC/parte 4/test3/Parser.c" "/home/igor/Scrivania/haskell/progettoLC/parte 4/test3/cmake-build-debug/CMakeFiles/test3.dir/Parser.c.o"
+  "/home/igor/Scrivania/haskell/progettoLC/parte 4/test3/Printer.c" "/home/igor/Scrivania/haskell/progettoLC/parte 4/test3/cmake-build-debug/CMakeFiles/test3.dir/Printer.c.o"
+  "/home/igor/Scrivania/haskell/progettoLC/parte 4/test3/Skeleton.c" "/home/igor/Scrivania/haskell/progettoLC/parte 4/test3/cmake-build-debug/CMakeFiles/test3.dir/Skeleton.c.o"
+  "/home/igor/Scrivania/haskell/progettoLC/parte 4/test3/Test.c" "/home/igor/Scrivania/haskell/progettoLC/parte 4/test3/cmake-build-debug/CMakeFiles/test3.dir/Test.c.o"
+  "/home/igor/Scrivania/haskell/progettoLC/parte 4/test3/env.c" "/home/igor/Scrivania/haskell/progettoLC/parte 4/test3/cmake-build-debug/CMakeFiles/test3.dir/env.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ parte 4/test3/cmake-build-debug/CMakeFiles/test3.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test3.dir/Absyn.c.o"
+  "CMakeFiles/test3.dir/env.c.o"
+  "CMakeFiles/test3.dir/Lexer.c.o"
+  "CMakeFiles/test3.dir/Parser.c.o"
+  "CMakeFiles/test3.dir/Printer.c.o"
+  "CMakeFiles/test3.dir/Skeleton.c.o"
+  "CMakeFiles/test3.dir/Test.c.o"
+  "test3.pdb"
+  "test3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
